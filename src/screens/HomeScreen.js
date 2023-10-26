3e99d56173521f73cbb477155dd0f5504fbb6b11
Travis Lamberte <travis@lamberte.com>
2023-10-26 09:56:26 -0700

    add Image Screen to app

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -8,13 +8,15 @@ const HomeScreen = ({navigation}) => {
       <Button 
         onPress={()=> navigation.navigate('Components')}
         title="Go to Components Demo"
-        />
+      />
       <Button 
         onPress={() => navigation.navigate('List')}
         title="Go to List Demo" 
-        />
-
-
+      />
+      <Button
+        onPress={() => navigation.navigate('Image')}
+        title="Go to Image Demo"
+      />
     </View>
     )
 };
